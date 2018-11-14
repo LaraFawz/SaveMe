19956b783c7b59fd936489443e5dad614e332cd8
Lara Fawz <lara.fawz@hotmail.com>
2018-11-14 10:21:59 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/lara/saveme/data/PetContract.java
@@ -0,0 +1,67 @@
+package com.example.lara.saveme.data;
+
+/**
+ * Created by Lara on 4/5/2018.
+ */
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * API Contract for the Pets app.
+ */
+
+public final class PetContract {
+
+    private PetContract() {}
+
+
+    public static final String CONTENT_AUTHORITY = "com.example.lara.saveme";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_PETS = "pets";
+
+
+    public static final class PetEntry implements BaseColumns {
+
+        /**
+         * The content URI to access the pet data in the provider
+         */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
+
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PETS;
+
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PETS;
+
+
+        public final static String TABLE_NAME = "pets";
+
+        public final static String _ID = BaseColumns._ID;
+
+
+        public final static String COLUMN_PET_NAME = "name";
+
+        public final static String COLUMN_PET_BREED = "breed";
+
+        public final static String COLUMN_PET_GENDER = "gender";
+
+        public final static String COLUMN_PET_WEIGHT = "weight";
+
+
+        public static final int GENDER_UNKNOWN = 0;
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
+
+        public static boolean isValidGender(int gender) {
+            if (gender == GENDER_UNKNOWN || gender == GENDER_MALE || gender == GENDER_FEMALE) {
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
